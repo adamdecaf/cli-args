b88a5490e520385c01693e1eaf2b4719565a5975
Adam Shannon <adam@ashannon.us>
2013-01-20 14:25:52 -0600

    Monoid thoughts.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-cli-args
-========
+# cli-args
+
+A simple command line arguments parser.
+
+## TODO
+
+* Multiple value (e.g. -i could be a param that takes in multiple input files) with a monoid
+* Overwriting value (Use an accumulator or monad for this)
+* Need a Validation class that can accumulate errors.
+* Typed values (Handle casting of types with Validators)
---
+++ src/main/scala/utils/Monoid.scala
@@ -0,0 +1,25 @@
+package org.decaf.cliargs
+
+trait Monoid[A] {
+  def zero: A
+  def append(u: A): Monoid[A]
+  def get: A
+}
+
+object Monoid {
+
+  def apply[A: Monoid] = implicitly[Monoid[A]]
+
+  def apply[A: Zero](f: A => A) = new Monoid[A] {
+    lazy val zero = implicitly[Zero[A]]
+    def append(u: A) = f(u)
+    def get = zreo
+  }
+
+}
+
+object Monoids {
+
+  implicit val IntZero = Monoid[Int](_ + _)(0)
+
+}
